675e1af46bb19689752feda5b56c84e0361ac710
Kaeli Lo <puiesabu@gmail.com>
2014-12-08 18:31:03 +0800

    Create thumbnail for profile_picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ActiveRecord::Base
   validates :device_id, uniqueness: true
 
   # for profile picture
-  has_attached_file :profile_picture
+  has_attached_file :profile_picture, :styles => { :thumb => "100x100>" }
   validates_attachment_file_name :profile_picture, :matches => [/png\Z/, /jpe?g\Z/]
 
   has_many :group
 
   def profile_picture_url
-    ActionController::Base.asset_host + self.profile_picture.url
+    ActionController::Base.asset_host + self.profile_picture.url(:thumb)
   end
 
   def as_json(options)
