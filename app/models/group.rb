2b1dfa52ee43883ce4cb88ab18d0b30205b965e7
Kaeli Lo <puiesabu@gmail.com>
2014-12-09 13:21:25 +0800

    Add location details in group json

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,11 @@
 class Group < ActiveRecord::Base
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   belongs_to :location
+
+  def as_json(options)
+    json = super
+
+    location = Location.find(self.location_id)
+    json.merge({"location" => location})
+  end
 end
