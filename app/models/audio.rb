1cf0948676c714ba877c234ad491bc41e0cb60b0
Kaeli Lo <puiesabu@gmail.com>
2014-12-10 15:54:46 +0800

    Add audioclip url in audio json

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,4 +1,13 @@
 class Audio < ActiveRecord::Base
   has_attached_file :audioclip
   validates_attachment_file_name :audioclip, :matches => [/m4a\Z/]
+
+  def audioclip_url
+    ActionController::Base.asset_host + self.audioclip.url
+  end
+
+  def as_json(options)
+    json = super
+    json.merge({"audioclip_url" => self.audioclip_url})
+  end
 end
